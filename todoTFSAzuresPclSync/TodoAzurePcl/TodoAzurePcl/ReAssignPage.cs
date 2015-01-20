826b9dc972c1aa094388fe076cf42432229fdfe7
iitr-ankur <iitr.ankur@gmail.com>
2015-01-20 17:38:51 +0530

    Adding missed out reassign page.cs

---
+++ todoTFSAzuresPclSync/TodoAzurePcl/TodoAzurePcl/ReAssignPage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace TodoAzurePcl
+{
+    public class ReAssignPage : ContentPage
+    {
+        public ReAssignPage(string taskId)
+        {
+            ListView users = new ListView();
+            users.ItemTemplate = new DataTemplate(typeof(TextCell));
+            users.ItemTemplate.SetBinding(TextCell.TextProperty, "Name");
+            users.ItemTemplate.SetBinding(TextCell.DetailProperty, "PhoneNum");
+            users.ItemsSource = App.MyContacts;
+            users.ItemSelected += (sender, e) =>
+                {
+                    if (e.SelectedItem != null)
+                    {
+                        var reassignTo = (Contact)e.SelectedItem;
+                        MessagingCenter.Send<ReAssignPage, Contact>(this, Constants.ReassignTag + taskId, reassignTo);
+                        users.SelectedItem = null;
+                        this.Navigation.PopAsync();
+                    }
+                };
+
+            
+            
+            //ActivityIndicator activityIndicator = new ActivityIndicator{
+            //    Color = Device.OnPlatform(Color.Black, Color.Default, Color.Default),
+            //    IsRunning = true,
+            //    VerticalOptions = LayoutOptions.Center};
+
+            this.Title = "Re-assign To:";
+            this.Content = new StackLayout() { Children = { users }, Orientation = StackOrientation.Vertical };
+
+        }
+    }
+}
